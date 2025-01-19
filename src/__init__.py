65d040e71023e21af96ff08793394bf05bc7c22d
mburuxx <njihiamburu.3@gmail.com>
2025-01-19 05:36:02 +0300

    Creating blueprints for auth and bookmarks

--- src/__init__.py
+++ src/__init__.py
@@ -1,24 +1,20 @@
 from flask import Flask
 import os
+from src.auth import auth
+from src.bookmarks import bookmarks
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
         app.config.from_mapping(
-            SECRWT_KEY = os.environ.get("SECRET_KEY")
+            SECRET_KEY = os.environ.get("SECRET_KEY")
         )
 
     else:
         app.config.from_mapping(test_config)
 
-    @app.get("/")
-
-    def index():
-        return "Welcome Home Again!"
-
-    @app.get("/hello")
-    def say_hello():
-        return {"message" : "Hello Developer!"}
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
 
     return app
